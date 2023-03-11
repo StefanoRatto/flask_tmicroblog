5f89cf4b57912164810217b876f29e9a1feb2c0b
StefanoRatto <stefano.ratto@gmail.com>
2023-03-11 15:42:48 -0600

    Chapter 3 completed.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Danny'}
+    posts = [
+        {
+            'author': {'username': 'Stefano'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Laura'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
